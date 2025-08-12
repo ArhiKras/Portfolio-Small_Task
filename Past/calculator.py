5180d2189b115755d2341ea6ad0920fe325c1278
ArhiKras <art.kras.iv@gmail.com>
2025-08-12 09:59:35 +0000

    DZ_2

--- Past/calculator.py
+++ Past/calculator.py
@@ -1,21 +1,20 @@
 # Калькулятор
 
-
 while True:
-  num1 = float(input("Введите первое число: "))
-  num2 = float(input("Введите второе число: "))
-  operation = input("Введите операцию (+, -, *, /): ")
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    operation = input("Введите операцию (+, -, *, /): ")
 
-  if operation == "+":
-    print(f"Результат: {num1 + num2}")
-  elif operation == "-":
-    print(f"Результат: {num1 - num2}")
-  elif operation == "*":
-    print(f"Результат: {num1 * num2}")
-  elif operation == "/":
-    if num2 != 0:
-      print(f"Результат: {num1 / num2}")
+    if operation == "+":
+        print(f"Результат: {num1 + num2}")
+    elif operation == "-":
+        print(f"Результат: {num1 - num2}")
+    elif operation == "*":
+        print(f"Результат: {num1 * num2}")
+    elif operation == "/":
+        if num2 != 0:
+            print(f"Результат: {num1 / num2}")
+        else:
+            print("Деление на ноль невозможно")
     else:
-      print("Деление на ноль невозможно")
-  else:
-    print("Неизвестная операция")
+        print("Неизвестная операция")
